05b770ed64126ba7dade1def9174c9c5b5a41e28
djjproject <djj9404@gmail.com>
2022-08-13 10:06:50 +0900

    fixes for no web ui.

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # start PowerManager Server
+cd /app/server
 mkdir -p /app/data/log
 nohup /app/server/PowerManagerServer --config /app/data/config.yml --log /app/data/log < /dev/null > /dev/null 2>&1 &
 
