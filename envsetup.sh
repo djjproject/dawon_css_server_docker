4a8883bb58bf7854163ba0804ef86088c8cce0b7
djjproject <djj9404@gmail.com>
2022-08-15 23:48:44 +0900

    refine envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+CONTAINER_NAME=test
+IMAGE_NAME=djjproject/dawon_css_server
+IMAGE_TAG=release-e1d011ae
+
 function build() {
     clean
-    docker build . -t test
+    docker build . -t $IMAGE_NAME:$IMAGE_TAG
 }
 
 function clean() {
-    docker rm -f test
-    docker rmi test
+    docker rm -f $IMAGE_NAME:$IMAGE_TAG
+    docker rmi $IMAGE_NAME:$IMAGE_TAG
+    docker rmi $IMAGE_NAME:latest
 }
 
 function test_build() {
     build
-    docker run -dit --restart unless-stopped --network host -v /opt/powermanager:/app/data --name test test
-    docker exec -it test /bin/bash
+    docker run -dit --restart unless-stopped --network host -v /opt/powermanager:/app/data --name $CONTAINER_NAME $IMAGE_NAME:$IMAGE_TAG
+    docker exec -it $CONTAINER_NAME /bin/bash
+}
+
+function push_image() {
+    build
+    docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
+    docker push $IMAGE_NAME:$IMAGE_TAG
+    docker push $IMAGE_NAME:latest
+    clean
 }
